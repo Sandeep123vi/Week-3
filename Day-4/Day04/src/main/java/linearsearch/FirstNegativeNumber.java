26ac5e6996a812eaf3a27d64f1bf666cf9f45ec8
Sandeep123vi <sandeepvishwakarna2822@gmail.com>
2025-02-05 19:00:51 +0530

    [Sandeep] code for finding first negative element

---
+++ Day-4/Day04/src/main/java/linearsearch/FirstNegativeNumber.java
@@ -0,0 +1,40 @@
+package linearsearch;
+
+import java.util.Scanner;
+
+public class FirstNegativeNumber {
+
+    // Method to Linear Search for the first negative number
+    public static int findFirstNegative(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements for array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        // Calling the method to find the first negative number
+        int index = findFirstNegative(arr);
+
+        //  the result
+        if (index != -1) {
+            System.out.println("First negative number found at index: " + index);
+        } else {
+            System.out.println("No negative number found in the array.");
+        }
+
+    }
+}
